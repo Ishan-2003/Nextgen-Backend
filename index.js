d9fa2a5c485863a324b53473a4da080e0ca08bd8
Ishan-2003 <21BCS022@nith.ac.in>
2024-11-04 23:34:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 const express = require('express')
 const dbConnect = require("./dbConnect")
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const authRouter = require("./routes/Authroute");
-const cookieparser = require('cookie-parser');
-const { notFound, errorHandler } = require("./middlewares/ErrorHandler");
-const productrouter = require('./routes/Productroute');
-const blogrouter = require('./routes/Blogroute');
-const prodcategoryrouter = require('./routes/ProdCategoryroute');
-const blogcategoryrouter = require('./routes/BlogCategoryroute');
-const brandrouter = require('./routes/Brandroute');
-const colorrouter = require('./routes/Colorroute');
-const morgan = require("morgan");
+// const authRouter = require("./routes/Authroute");
+// const cookieparser = require('cookie-parser');
+// const { notFound, errorHandler } = require("./middlewares/ErrorHandler");
+// const productrouter = require('./routes/Productroute');
+// const blogrouter = require('./routes/Blogroute');
+// const prodcategoryrouter = require('./routes/ProdCategoryroute');
+// const blogcategoryrouter = require('./routes/BlogCategoryroute');
+// const brandrouter = require('./routes/Brandroute');
+// const colorrouter = require('./routes/Colorroute');
 require('dotenv').config();
 const PORT = process.env.PORT;
+const morgan = require("morgan");
 
 dbConnect();
 app.get("/",(req,res)=>{res.send("running on and on and on...");});
 app.use(cors());
 app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieparser());
-app.use("/api/User",authRouter);
-app.use("/api/Product",productrouter);
-app.use("/api/Blog",blogrouter);
-app.use("/api/Category",prodcategoryrouter);
-app.use("/api/BlogCategory",blogcategoryrouter);
-app.use("/api/Brand",brandrouter);
-app.use("/api/Color",colorrouter);
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(cookieparser());
+// app.use("/api/User",authRouter);
+// app.use("/api/Product",productrouter);
+// app.use("/api/Blog",blogrouter);
+// app.use("/api/Category",prodcategoryrouter);
+// app.use("/api/BlogCategory",blogcategoryrouter);
+// app.use("/api/Brand",brandrouter);
+// app.use("/api/Color",colorrouter);
 
-app.use(notFound);
-app.use(errorHandler);
+// app.use(notFound);
+// app.use(errorHandler);
 
 
 app.listen(PORT,()=>{
